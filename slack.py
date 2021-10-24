8ab462ad343eae96bbd564a7e34a353219a3a0cc
itaynvn <itay.anavian@cnvrg.io>
2021-10-24 13:42:39 +0300

    add args

--- slack.py
+++ slack.py
@@ -18,20 +18,18 @@ def slack(webhook_url,message):
          }
         ]
     )
-    
-#TODO --- ADD your imports Classes and methods
 
 if __name__ == '__main__':
     parser = ArgumentParser(add_help=False)
     # TODO --- ADD your parameters
     parser.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    parser.add_argument('-m', '--message', action='store', help="the string that will be sent as message to the slack channel",
-                      required=True, type=str)
+    parser.add_argument('-m', '--message', action='store', help="the string that will be sent as message to the slack channel", required=True, type=str)
+    parser.add_argument('--project_dir', action='store', dest='project_dir', help="""--- For inner use of cnvrg.io ---""")
+    parser.add_argument('--output_dir', action='store', dest='output_dir', help="""--- For inner use of cnvrg.io ---""")
 
     args = parser.parse_args()
-
+    print("these are the arguments sent:")
     print(args)
-    print("TEMPLATE AI COMPONENT WORKS!!")
     #TODO --- execut your AI Component code
     webhook_url = os.getenv("SLACK_WEBHOOK_URL")
     slack(webhook_url,args.message)
